02e8d8e7e40919596f7fc3adc45b8d8c26393744
Dan <dannyhudetz@gmail.com>
2020-07-31 13:50:53 -0500

    7/31 Meeting Demonstration
    (+) Simultaniously, when clicking a state, the districts of that state are loaded and the map is zoomed and adjusted to look at that state.
    (+) When zooming back out after in district view, state view is automatically set. But zooming in from state view does not bring you to district view, that requires a state to be selected.
    (+) Pop-ups to again allow for information about a district to be displayed
    (+) Changing the colors of individual districts

--- main.js
+++ main.js
@@ -6,25 +6,66 @@ window.onresize = function()
   setTimeout(map.updateSize(), 200);
 }
 
+// Variables for zooming in on a selected state
+var stateClicked = false;
+var coordinateRecieved = null;
+var stateMode = true;
 var numDistrictsJson;
 //get json of number of districts for each state
 $.getJSON("./data/numberOfDistricts.json", function(json) {
   numDistrictsJson=json;
 });
 
+const statesGeoJSON = new ol.layer.VectorImage({
+  source: new ol.source.Vector({
+    url: './data/states.json',
+    format: new ol.format.GeoJSON()
+  }),
+  visible: true,
+  title: 'StatesGeoJSON'
+})
+
 const zoomCutOff=5;
 var map;
 //const fs = require('fs');
 
+var view = new ol.View({
+  center:[-10692079, 4855956],
+  zoom: 1,
+  maxZoom: 10,
+  //minZoom: 1,
+  extent: [-20000000,1420000,-5130000,12750000]
+});
+
+var container = document.getElementById('popup');
+var content = document.getElementById('popup-content');
+var closer = document.getElementById('popup-closer');
+
+var overlay;
+
 function init(){
+  //create overlay for popup messages
+  overlay = new ol.Overlay({
+    element: container,
+    autoPan: true,
+    autoPanAnimation: {
+      duration: 250,
+    }
+  });
+
+  closer.onclick = function () {
+    overlay.setPosition(undefined);
+    closer.blur();
+    return false;
+  };
+
+  // Still need to research what attributions i actually have to make
+  var attributions =
+  '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
+  '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
   map = new ol.Map({
-    view: new ol.View({
-      center:[-10692079, 4855956],
-      zoom: 1,
-      maxZoom: 10,
-      //minZoom: 1,
-      extent: [-20000000,1420000,-5130000,12750000]
-    }),
+    view: view,
+    overlays: [overlay],
     layers: [
       new ol.layer.Tile({
         source: new ol.source.OSM()
@@ -33,66 +74,39 @@ function init(){
     target: 'map'
   })
 
-  //Vector layers
-  /*const districtsGeoJSON = new ol.layer.VectorImage({
-    source: new ol.source.Vector({
-      url: 'https://theunitedstates.io/districts/cds/2012/NY-2/shape.geojson',
-      format: new ol.format.GeoJSON()
-    }),
-    visible: true,
-    title: 'DistrictsGeoJSON'
-  })*/
-
-  const statesGeoJSON = new ol.layer.VectorImage({
-    source: new ol.source.Vector({
-      url: './data/states.json',
-      format: new ol.format.GeoJSON()
-    }),
-    visible: true,
-    title: 'StatesGeoJSON'
-  })
-
-  //map.addLayer(districtsGeoJSON);
-  const openStreetMapHumanitarian = new ol.layer.Tile({
-    source: new ol.source.OSM({
-      url: 'http://tile.stamen.com/toner/{z}/{x}/{y}.png'
-    }),
-    visbile: true,
-    title: 'OSMHumanitarian'
-  })
-  //map.removeLayer();
-
-  var select = new ol.interaction.Select({
-    //condition: ol.click
-  });
+  var select = new ol.interaction.Select();
   map.addInteraction(select);
-    select.on('select', function(e) {
-      document.getElementById('status').innerHTML = '&nbsp;' +
-          e.target.getFeatures().getLength() +
-          ' selected features (last operation selected ' + e.selected.length +
-          ' and deselected ' + e.deselected.length + ' features)';
+  select.on('select', function (e) {
+    stateClicked =true;
+    attemptSelectState();
   });
+
+  map.on('click', function(e){
+    var size = map.getSize();
+    var districtView = new ol.View({
+      center:e.coordinate,
+      zoom: 7,
+      maxZoom: 10,
+      minZoom: 5,
+      extent: [-20000000,1420000,-5130000,12750000]
+    });
+    coordinateRecieved = e.coordinate;
+    attemptSelectState();
+  })
   //Loading/unloading states and districts depending on zoom level
   var currZoom = map.getView().getZoom();
   map.addLayer(statesGeoJSON);
   map.on('moveend', function(e) {
     var newZoom = map.getView().getZoom();
     if (currZoom != newZoom) {
-      if(newZoom >= zoomCutOff && currZoom < zoomCutOff){
-        map.removeLayer(statesGeoJSON);
-        loadStateDistricts('NY');
-      }
-      else if(newZoom < zoomCutOff && currZoom >= zoomCutOff){
+      if(!stateMode && newZoom < zoomCutOff && currZoom >= zoomCutOff){
         unloadStateDistricts();
         map.addLayer(statesGeoJSON);
+        stateMode= true;
       }
       currZoom = newZoom;
     }
   });
-
-  map.on('click', function(e){
-    console.log(e.coordinate);
-  })
   //toggleFullScreen("#map");
   //toggleFullScreen("#map");
 }
@@ -114,20 +128,55 @@ var loadedDistricts = new Array(55);
 of a state*/
 function loadStateDistricts(state){
   var numDistricts=numDistrictsJson[state];
-  console.log(numDistricts);
   //Loop through the districts for this state
   loadedDistricts = new Array(numDistricts);
   for(var i = 1; i<=numDistricts; i++){
+    var strokeColor,fillColor;
+    if (i%3!=0){
+      strokeColor = '#f00';
+      fillColor = 'rgba(255,0,0,0.2)';
+    }
+    else {
+      strokeColor = '#0f0';
+      fillColor = 'rgba(0,255,0,0.2)';
+    }
     const districtsGeoJSON = new ol.layer.VectorImage({
       source: new ol.source.Vector({
         url: 'https://theunitedstates.io/districts/cds/2012/'+state+'-'+i.toString()+'/shape.geojson',
         format: new ol.format.GeoJSON()
       }),
       visible: true,
-      title: 'DistrictsGeoJSON'
+      title: 'DistrictsGeoJSON',
+      style: new ol.style.Style({
+        stroke: new ol.style.Stroke({
+          color: strokeColor,
+          width: 1,
+        }),
+        fill: new ol.style.Fill({
+          color: fillColor,
+        }),
+      })
     })
     loadedDistricts[i-1]=districtsGeoJSON;
     map.addLayer(districtsGeoJSON);
+  }
+}
+
+function attemptSelectState(){
+  if(stateClicked && coordinateRecieved!=null){
+    if(stateMode==true){
+      map.removeLayer(statesGeoJSON);
+      loadStateDistricts('IL');
+      view.setCenter(coordinateRecieved);
+      view.setZoom(6);
+      coordinateRecieved=null;
+      stateClicked=false;
+      stateMode=false;
+    }
+    else{
+      content.innerHTML = '<p>This district has many reports of 2 hour+ lines</p>';
+      overlay.setPosition(coordinateRecieved);
+    }
   }
 }
 
